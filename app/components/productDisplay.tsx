3c9f8d12cdfceb447e1626f24e2ed16ffc85ccd6
akglitch <mcaninakwa@gmail.com>
2024-05-07 22:42:54 -0700

    added background color to add to cart button and resized the cards

--- app/components/productDisplay.tsx
+++ app/components/productDisplay.tsx
@@ -30,7 +30,7 @@ const ProductDisplay: React.FC = () => {
                 <span className="mr-2 ml-3 rounded bg-yellow-200 px-2 py-0.5 text-xs font-semibold">5.0</span>
               </div>
             </div>
-            <a href="#" className="flex items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+            <a href="#" className="flex items-center bg-gray-700 justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
               <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
